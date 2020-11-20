71478dcecd31d9cbca4076d58d3392d973ee9220
tangxinli <jeff.li@chintec.net>
2020-11-20 12:16:50 +0800

    author:jeff work:init project date:2020年11月17日13:25:04

--- miaomu-common/src/main/java/com/miaomu/common/entity/response/ResultResponse.java
+++ miaomu-common/src/main/java/com/miaomu/common/entity/response/ResultResponse.java
@@ -1,7 +1,15 @@
 package com.miaomu.common.entity.response;
 
+import com.miaomu.common.enums.ResponseEnum;
 import lombok.Data;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
 
+import java.util.Objects;
+
+/**
+ * @author tangxinli
+ */
 @Data
 public class ResultResponse {
 
@@ -21,24 +29,39 @@ public class ResultResponse {
     public ResultResponse() {
     }
 
-    public static ResultResponse successful(String message, Object t) {
-        return new ResultResponse(200, message, t);
+    public static ResultResponse successful(Integer status, String message, Object t) {
+        return new ResultResponse(ResponseEnum.RESPONSE_ENUM_SUCCESS.getCode(), message, t);
     }
 
+    public static ResultResponse successful(String message, Object t) {
+        return successful(ResponseEnum.RESPONSE_ENUM_SUCCESS.getCode(), message, t);
+    }
+
+
     public static ResultResponse successful(Object t) {
-        return new ResultResponse(200, "SUCCESS", t);
+        return successful(ResponseEnum.RESPONSE_ENUM_SUCCESS.getCode(), "SUCCESS", t);
     }
 
     public static ResultResponse error(Integer status, String message, Object t) {
-        return new ResultResponse(status, message, t);
+        return error(status, message, t);
     }
 
     public static ResultResponse error(String message, Object t) {
-        return new ResultResponse(40001, message, t);
+        return error(ResponseEnum.RESPONSE_ENUM_ERROR.getCode(), message, t);
     }
 
     public static ResultResponse error(Object t) {
-        return new ResultResponse(40001, "FAILURE", t);
+        return new ResultResponse(ResponseEnum.RESPONSE_ENUM_USER_ERROR.getCode(), "FAILURE", t);
     }
 
+    public static ResultResponse error(Integer status, BindingResult result, Object t) {
+        if (result.hasErrors()) {
+            if (StringUtils.isEmpty(status)) {
+                return error(ResponseEnum.RESPONSE_ENUM_ERROR.getCode(), Objects.requireNonNull(result.getFieldError()).getDefaultMessage(), t);
+            }
+            return error(status, Objects.requireNonNull(result.getFieldError()).getDefaultMessage(), t);
+        }
+        return error(ResponseEnum.RESPONSE_ENUM_ERROR.getCode(), "FAILURE", t);
+    }
 }
+
